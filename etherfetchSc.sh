5a13cfb02fb085c1e080168bd4212d64d046a3bc
k0f <krystof.pesek@gmail.com>
2015-03-05 02:59:46 +0100

    working multiline version

--- etherfetchSc.sh
+++ etherfetchSc.sh
@@ -15,8 +15,11 @@ rm /tmp/lang
 rm /tmp/lang2
 mkfifo /tmp/lang
 mkfifo /tmp/lang2
-(while true; do curl -vs $@/export/txt > /tmp/lang && sleep 2s ; done)&
-tail -f /tmp/lang 2> >(grep -v truncated >&2) | grep -v -e "^\/\/.*" --line-buffered | sclang
+(while true; do curl -vs $@/export/txt | grep -v -e "\/\/.*" > /tmp/lang && sleep 2s ; done)&
+(while true; do echo -n "`cat /tmp/lang`" | tr -d '\n' > /tmp/lang2 &&  echo -e "\r" > /tmp/lang2 && sleep 2s ;done)&
+sleep 10s && tail -f /tmp/lang2 | sclang
+
+#tail -f /tmp/lang 2> >(grep -v truncated >&2) | grep -v -e "^\/\/.*" --line-buffered | sclang
 #(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | sed 'Wf a\'/n'/g')
 #(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | awk 'BEGIN {RS=""}{gsub(/\n/,"",$0); print $0}' | supercolliderJs)
 #(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | awk '{printf "%s",$0} END {print ""}' | supercolliderJs)
