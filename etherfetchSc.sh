a88cff00c091ff2edd232b7130017ffcea9a39e0
k0f <krystof.pesek@gmail.com>
2015-03-05 03:02:43 +0100

    cleanup

--- etherfetchSc.sh
+++ etherfetchSc.sh
@@ -2,7 +2,7 @@
 
 # usage: 
 # ./etherfetchSc.sh http://etherpad.address.com/xxxyyyyzzz
-# script to parse etherpad and pipe it live into supercollider.js
+# script to parse etherpad and pipe it live into supercollider
 # multiuser livecoding session
 # requires curl
 
@@ -18,10 +18,3 @@ mkfifo /tmp/lang2
 (while true; do curl -vs $@/export/txt | grep -v -e "\/\/.*" > /tmp/lang && sleep 2s ; done)&
 (while true; do echo -n "`cat /tmp/lang`" | tr -d '\n' > /tmp/lang2 &&  echo -e "\r" > /tmp/lang2 && sleep 2s ;done)&
 sleep 10s && tail -f /tmp/lang2 | sclang
-
-#tail -f /tmp/lang 2> >(grep -v truncated >&2) | grep -v -e "^\/\/.*" --line-buffered | sclang
-#(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | sed 'Wf a\'/n'/g')
-#(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | awk 'BEGIN {RS=""}{gsub(/\n/,"",$0); print $0}' | supercolliderJs)
-#(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | awk '{printf "%s",$0} END {print ""}' | supercolliderJs)
-#(tail -f /tmp/lang | grep -v -e "^\/\/.*" --line-buffered | awk 1 OSR=' ' | sed -u 'N;s/\n//')
-# /tmp/preproc | supercolliderJs
